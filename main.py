851343908d48a75f1b8037d9c78b514d3d42594d
Gauri Pandey <gauri22pandey@gmail.com>
2024-08-23 19:35:54 +0530

    Automated message file

---
+++ main.py
@@ -0,0 +1,19 @@
+import pywhatkit as kit
+from datetime import datetime, timedelta
+
+# Define the phone number with the country code, e.g., +1234567890
+phone_number = "+919305190621"
+
+# Define the message you want to send
+message = "Hello, this is an automated message sent using Python!"
+
+# Define the time to send the message (current time + 1 minute)
+now = datetime.now()
+send_time = now + timedelta(minutes=1)
+hours = send_time.hour
+minutes = send_time.minute
+
+# Send the message
+kit.sendwhatmsg(phone_number, message, hours, minutes)
+
+print("Message scheduled to be sent.")
